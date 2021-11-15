2d41e3bd327c5fd154688e1d4c0b26cde3801fdb
Arafat <arafat97alim@gmail.com>
2021-11-15 20:04:14 +0530

    Added homepage content.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+
+const { Title } = Typography;
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24th Vloume" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" />
+        </Col>
+      </Row>
+    </>
   );
 };
 
