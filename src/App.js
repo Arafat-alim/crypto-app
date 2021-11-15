c1ffe448c948c673bc64db242e6472c52592fc30
Arafat <arafat97alim@gmail.com>
2021-11-15 19:13:09 +0530

    added more about sidebar and navbar styling.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React from "react";
-import { Swtich, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import { NavBar } from "./components";
+import {
+  NavBar,
+  Homepage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className="app">
-      <div className="navbar">
-        <NavBar />
-      </div>
-      <div className="main"></div>
-      <div className="footer"></div>
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <NavBar />
     </div>
-  );
-};
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/exchanges">
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
+    </div>
+    <div className="footer"></div>
+  </div>
+);
 
 export default App;
